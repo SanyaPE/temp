1fa84c08093322f747d525b47ba71ef133bb6a26
Ilya Kozak <kozak@tut.by>
2023-02-01 17:44:43 +0300

    refactor: Layout component

--- async-race/src/components/Layout/layout.ts
+++ async-race/src/components/Layout/layout.ts
@@ -3,6 +3,7 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 import { header } from '../../templates/header';
 import { mainElement } from '../../templates/mainElement';
+import { footer } from '../../templates/footer';
 
 export default class Layout {
     body: HTMLElement | null = null;
@@ -10,7 +11,7 @@ export default class Layout {
     init() {
         this.body = document.querySelector('body') as HTMLElement;
         this.createHeader();
-        this.createMain();
+        this.createMainElement();
         this.createFooter();
     }
 
@@ -18,21 +19,11 @@ export default class Layout {
         (this.body as HTMLElement).insertAdjacentHTML('beforeend', header);
     }
 
-    createMain() {
+    createMainElement() {
         (this.body as HTMLElement).insertAdjacentHTML('beforeend', mainElement);
     }
 
     createFooter() {
-        const footer = `<footer class="footer">
-        <div class="container">
-            <div class="footer__content">
-                <div class="footer__copy">&copy;</div>
-                <div class="footer__year">2022</div>
-                <a href="https://github.com/SanyaPE" class="footer__github nav__link" target="_blank">sanyape</a>
-                <a href="https://rs.school/js/" class="footer__rss" target="_blank"></a>
-            </div>
-        </div>
-    </footer>`;
         (this.body as HTMLElement).insertAdjacentHTML('beforeend', footer);
     }
 }
---
+++ async-race/src/templates/footer.ts
@@ -0,0 +1,10 @@
+export const footer = `<footer class="footer">
+<div class="container">
+    <div class="footer__content">
+        <div class="footer__copy">&copy;</div>
+        <div class="footer__year">2022</div>
+        <a href="https://github.com/SanyaPE" class="footer__github nav__link" target="_blank">sanyape</a>
+        <a href="https://rs.school/js/" class="footer__rss" target="_blank"></a>
+    </div>
+</div>
+</footer>`;
